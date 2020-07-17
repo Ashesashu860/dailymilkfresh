8514d138ad22e4f9f14757a98f92cad15bf89a65
Ashutosh <ashutoshforjobs@gmail.com>
2020-07-18 01:50:06 +0530

    how it wroks finished

--- src/pages/HowItWorks/HowItWorks.js
+++ src/pages/HowItWorks/HowItWorks.js
@@ -11,6 +11,7 @@ import TimelineDot from "@material-ui/lab/TimelineDot";
 import Paper from "@material-ui/core/Paper";
 import { howItWorksData } from "./HowItWorksData";
 import { colors } from "../../theme";
+import "./how_it_works.css";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -23,67 +24,83 @@ export default function HowItWorks() {
 
   return (
     <div className="wrapper_image_grey unbound_wrapper">
-      <h2 style={{ color: colors.primaryDark }} className="padding">
-        How it Works
-      </h2>
-      <Timeline align="alternate">
-        {howItWorksData.map((item, index) => (
-          <TimelineItem>
-            <TimelineSeparator>
-              <TimelineDot
-                style={{
-                  backgroundColor: colors.primary,
-                  borderWidth: "1px",
-                }}
-              >
-                {index < 6 ? (
-                  <Avatar
+      <Grid container justify="center" alignItems="center" direction="column">
+        <h3 style={{ color: colors.primaryDark }} className="padding">
+          How it Works
+        </h3>
+        <label style={{ color: colors.grey.dark, paddingBottom: "2vw" }}>
+          Fresh Cow's Milk from Farm to Table in Few Hours
+        </label>
+        <Timeline align="alternate">
+          {howItWorksData.map((item, index) => (
+            <TimelineItem className="timeline_item">
+              <TimelineSeparator className="full_width_on_small">
+                <TimelineDot
+                  style={{
+                    backgroundColor: colors.primary,
+                    borderWidth: "1px",
+                  }}
+                  className="timeline_dot"
+                >
+                  {index < 6 ? (
+                    <Avatar
+                      style={{
+                        height: "20vh",
+                        width: "20vh",
+                      }}
+                      alt="Remy Sharp"
+                      src={require(`../../assets/how_it_works/${item.icon}`)}
+                    />
+                  ) : (
+                    <p className="padding">{item.data}</p>
+                  )}
+                </TimelineDot>
+                {index < 6 && (
+                  <Paper
+                    elevation={3}
+                    className={`display_on_small ${classes.paper}`}
                     style={{
-                      height: "20vh",
-                      width: "20vh",
-                    }}
-                    alt="Remy Sharp"
-                    src={require(`../../assets/how_it_works/${item.icon}`)}
-                  />
-                ) : (
-                  <p
-                    style={{
-                      fontSize: "1vw",
-                      textAlign: "left",
-                      padding: "30px",
+                      backgroundColor: colors.primary,
                     }}
                   >
-                    {item.data}
-                  </p>
+                    <h6>{item.title}</h6>
+                    <p>{item.data}</p>
+                  </Paper>
                 )}
-              </TimelineDot>
-              <TimelineConnector
-                style={{ backgroundColor: colors.primary, height: "30px" }}
-              />
-            </TimelineSeparator>
-            <TimelineContent>
-              <Paper
-                elevation={3}
-                className={classes.paper}
-                style={{
-                  margin: "auto",
-                  minWidth: "100px",
-                  backgroundColor: colors.primary,
-                }}
-              >
-                <p
-                  style={{
-                    fontSize: "1vw",
-                    textAlign: "left",
-                  }}
-                >
-                  {item.data}
-                </p>
-              </Paper>
-            </TimelineContent>
-          </TimelineItem>
-        ))}
-      </Timeline>
+                {index < 6 && (
+                  <TimelineConnector
+                    style={{ backgroundColor: colors.primary, height: "30px" }}
+                  />
+                )}
+              </TimelineSeparator>
+              {index < 6 ? (
+                <TimelineContent className="display_on_big">
+                  <Paper
+                    elevation={3}
+                    className={classes.paper}
+                    style={{
+                      margin: "auto",
+                      minWidth: "100px",
+                      backgroundColor: colors.primary,
+                    }}
+                  >
+                    <h6>{item.title}</h6>
+                    <p
+                      style={{
+                        fontSize: "1vw",
+                      }}
+                    >
+                      {item.data}
+                    </p>
+                  </Paper>
+                </TimelineContent>
+              ) : (
+                <TimelineContent className="display_on_big"></TimelineContent>
+              )}
+            </TimelineItem>
+          ))}
+        </Timeline>
+      </Grid>
     </div>
   );
 }
